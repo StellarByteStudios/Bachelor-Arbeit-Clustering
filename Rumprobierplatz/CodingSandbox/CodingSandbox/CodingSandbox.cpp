783a6c07f6247a2d9ef378642af07858c40a13cb
StellarByteStudios <krollmann.carsten@gmail.com>
2023-11-21 11:06:09 +0100

    Clean Main
    Main-Methode etwas aufgeräumt für die Übersicht. Wurden nur Kommentare gelöscht

--- Rumprobierplatz/CodingSandbox/CodingSandbox/CodingSandbox.cpp
+++ Rumprobierplatz/CodingSandbox/CodingSandbox/CodingSandbox.cpp
@@ -12,22 +12,8 @@
 int main()
 {
     std::cout << "Hello World!\n";
-    //std::cout << "Current path is " << std::filesystem::current_path() << '\n';
 
     std::string fileName = "Points.txt";
-
-
-    
-
-
-    //std::stringstream pathStream;
-
-    //pathStream << std::filesystem::current_path() << fileName;
-
-    //std::cout << "Current path is " << std::filesystem::current_path() << '\n';
-    //std::cout << fileStringStream.str() << endl;;
-
-    //const char* path;
 
     Printer* printer = new Printer();
     PointParser* parser = new PointParser(fileName);
@@ -39,39 +25,6 @@ int main()
         printer->printnl(points->at(i).toString());
     }
 
-    //Reader* reader = new Reader(fileName);
-    //BetterReader* reader = new BetterReader(fileName);
-
-    /*
-
-    // Knoten erzeugen und Ausgeben
-    double coords[] = { 1.0, 2.0, 3.0 };
-    std::cout << coords[0] << std::endl;
-    std::cout << coords[1] << std::endl;
-    std::cout << coords[2] << std::endl;
-    ColoredPoint* point = new ColoredPoint(3, coords, RED);
-    */
-
-    //printer->printnl(point->toString().c_str());
-
-
-    
-    //printer->printnl("Printing via Printer");
-    //printer->printnl(fileName);
-
-    //char* fileInput = reader->readFile();
-
-    //printer->printnl(fileInput);
-
-    
-    /*
-    printer->printWithCounter("Printing via Object");
-    printer->printWithCounter("Printing again");
-    printer->printWithCounter("Printing again");
-    printer->printWithCounter("Printing again");
-    printer->printWithCounter("Printing again");
-    printer->printWithCounter("Printing again");
-    */
     //delete printer;
     //delete reader;
     //delete fileInput;
