f057a3ce1d85cf696d0a1c1e3ff2cb6c37bcac10
Carsten Krollmann <82361848+StellarByteStudios@users.noreply.github.com>
2023-11-27 08:25:37 -0800

    Code from old Project Migrated + Make Structure
    Habe den Code vom Rumprobierplatz rübergeschoben, welcher schon einsatzbereit war.
    
    Zusätzlich habe ich in Eclipse gefunden, wie ich das Make direkt im richtigen Ordner machen kann. Dafür habe ich das Makefile angepasst
    
    Zusätzlich noch die Warning von #Pragma once gefixt

--- Multicolor-Fair-Clusterin/ClusteringMain.cpp
+++ Multicolor-Fair-Clusterin/ClusteringMain.cpp
@@ -1,6 +1,35 @@
 #include <iostream>
 
+#include "ColoredPoint.h"
+#include "PointParser.h"
+
 int main(int argc, char **argv) {
-	std::cout << "Hello World!" << std::endl;
-	return 0;
+	std::cout << "Hello World!\n";
+
+	std::string fileName = "HandmadePoints.txt";
+
+	//Printer printer = Printer();
+	PointParser parser =  PointParser(fileName);
+
+	vector<ColoredPoint>* points = parser.parseFile();
+
+	for (int i = 0; i < (int) points->size(); i++)
+	{
+		std::cout << i << ": " << points->at(i).toString() << endl;;
+	}
+
+	// vector für Punkte wieder Freigeben
+	delete points;
+
+	std::cout << "\nTesting Distance\n";
+
+	double coord1[] = { 0, 2 };
+	ColoredPoint point1 = ColoredPoint(2, RED, coord1);
+
+	double coord2[] = { 0, 0 };
+	ColoredPoint point2 = ColoredPoint(2, BLUE, coord2);
+
+	std::cout << "Distance: " << point1.distTo(point2) << endl;
+
+	std::cout << "End Program" << std::endl;
 }
---
+++ Multicolor-Fair-Clusterin/PointParser.cpp
@@ -0,0 +1,92 @@
+#include "PointParser.h"
+#include <iostream> // std::cout; std::endl
+#include <fstream>  // ifstream (Dateieneinlesen)
+
+
+
+PointParser::PointParser(string path)
+{
+	this->path = path;
+}
+
+PointParser::~PointParser()
+{
+	cout << "PointParser is deleted" << endl;
+}
+
+vector<ColoredPoint>* PointParser::parseFile()
+{
+    // File aufmachen
+    ifstream file = ifstream(this->path, ios::in);
+
+    // Vector erstellen als Dynamische Datenstruktur
+    vector<ColoredPoint>* points = new vector<ColoredPoint>;
+
+
+    // Fehler falls File nicht offen
+    if (!file.good())
+    {
+        std::cout << "ERROR while opening File " << path << std::endl;
+        return nullptr;
+    }
+
+    // Punktez�hler
+    int numberOfPoints = 0;
+
+    // St�ck f�r St�ck durch das File gehen und einlesen
+    while (!file.eof() && numberOfPoints < MAXPOINTS)
+    {
+        // Zeile als String holen
+        string line;
+        std::getline(file, line);
+
+        // Gunddaten anlegen
+        int dim;
+        int colorAsInt;
+        Pointcolor color;
+
+        // Grunddaten aus dem String holen
+        size_t pos = 0;
+        // Dimensionen
+        pos = line.find(',');
+        dim = atoi(line.substr(0, pos).c_str());
+        line.erase(0, pos + 1);
+        // Farbe
+        pos = line.find(',');
+        colorAsInt = atoi(line.substr(0, pos).c_str());
+        line.erase(0, pos + 1);
+        color = static_cast<Pointcolor>(colorAsInt);
+
+        // Abbruchfall f�r die letzte Zeile
+        if (dim < 1)
+        {
+            break;
+        }
+
+        // Array f�r Punkte anlegen
+        double* coords = new double[dim];
+
+        // Einzelne Koordinaten parsen
+        for (int i = 0; i < dim; i++)
+        {
+            pos = line.find(',');
+            double temp = atof(line.substr(0, pos).c_str());
+            line.erase(0, pos + 1);
+            coords[i] = temp;
+        }
+
+        // Alles zu einem Punkt zusammenfassen
+        ColoredPoint temp = ColoredPoint(dim, color, coords);
+
+        // In den Vector pushen
+        points->push_back(temp);
+
+        // Koordinaten wieder Freigeben
+        delete coords;
+
+        // Z�hler erh�hen
+        numberOfPoints++;
+    }
+
+	return points;
+}
