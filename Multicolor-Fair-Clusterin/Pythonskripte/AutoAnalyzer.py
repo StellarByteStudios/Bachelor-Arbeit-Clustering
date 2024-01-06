302e6ed4740bd1c961ac593b1c1e92f6518d5e4c
Carsten Krollmann <82361848+StellarByteStudios@users.noreply.github.com>
2024-01-06 10:54:06 +0100

    First Shellcommand run by Python
    Ich kann jetzt mit meinem Python-Skript Befehle in der Shell ausführen, also auch mein Bash-Skript aufrufen.
    Ich bin von os auf subprocess umgestiegen, da ich nur bei subprocess die Shell genau angeben kann und in der Default-Shell ist nicht kompatibel mit der Syntax meines Bash-Skriptes

--- Multicolor-Fair-Clusterin/Pythonskripte/AutoAnalyzer.py
+++ Multicolor-Fair-Clusterin/Pythonskripte/AutoAnalyzer.py
@@ -4,17 +4,39 @@ Skript for automate feeding of the algorithms and analyzing their data
 """
 
 import Points
-import os
+import subprocess
 
 
 def main():
+    
+    # Parameter (Später noch über schleifen)
+    inputfile = "Data/Subsamples/bank/bankSample-0.csv"
+    outputfolder = "Data/OutputData/Autoanalyzer/bank/Sample0/"
+    outputfile = "bank0"
+    maxCluster = 100
+    
+    # Shellcommand zusammensetzen
+    command = f"cd .. && ./AlgFeeder.sh -i {inputfile} -o {outputfolder} -n {outputfile} -c {maxCluster}"
+    
+    #print(command)
+    
+    # Prozess erzeugen
+    process = bash_command(command)
+    # Prozess starten
+    process.communicate()
+    
+    # Wenn man die Ausgabe als String haben will
+    # outputString = process.communicate()[0]
+
+    # TO-DO: Analyse der Radien
     
     return
 
 
 
-
-
+# Kapselung des Bashcommands, der das mit der Shell regelt
+def bash_command(cmd): 
+    return subprocess.Popen(cmd, shell=True, executable='/bin/bash')
 
 
 
