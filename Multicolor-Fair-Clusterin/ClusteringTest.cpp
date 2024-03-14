c912a18a3faf59e760f9da05b5f426ae134bb87e
StellarByteStudios <krollmann.carsten@gmail.com>
2024-03-14 11:23:24 +0100

    Split main code into test and API
    Damit ich die Methoden Testen kann und gleichzeitig die API für die Pythonskripte nicht kaputt mache habe ich das ganze auseinander getrennt
    
    Nebenbei habe ich auch die Binaries der Tests gelöscht und sie in gitignore beigefügt

---
+++ Multicolor-Fair-Clusterin/ClusteringTest.cpp
@@ -0,0 +1,235 @@
+#include <iostream> // std::cout; std::endl
+#include <fstream>  // ifstream (Dateien ein/auslesen)
+#include <string>
+
+#include "ColoredPoint.h"
+#include "PointParser.h"
+#include "Gonzalez.h"
+#include "FairlettFinder.h"
+#include "RedCenterClustering.h"
+
+int main(int argc, char *argv[]) {
+
+	/* ==== getting Commandlinearguments ==== //
+	* Format:
+	*	1) Inputfile Path 
+	*	2) Outputfile Path
+	*	3) number of Clusters
+	*	[4) later: kind of Algorithm]
+	*/
+
+	/*
+	cout << "Number of Arguments: " << argc-1 << endl;
+	cout << "Name of Programm: " << argv[0] << endl;;
+	cout << "Now the Arguments: " << endl;
+	for(int i = 1; i < argc; i++){
+		cout << "\t- " << argv[i] << endl;
+	}*/
+ 
+	if (argc < 4)
+	{
+		cout << "Not enough Arguments\nneeded: \"inputFilePath\" \"outputFilePath\" numOfClusters" << endl;
+		return -1;
+	}
+	
+	string inputFileName = argv[1];//"Data/RandomGenerated/Points2D.txt";
+	string outputFileName = argv[2];//"Data/OutputData/ClusteredData/ClusterOut2DNew.csv";
+	int numberOfCluster = stoi(argv[3]);
+
+	// ==== Parsing of Points ==== //
+	PointParser parser =  PointParser(inputFileName);
+	vector<ColoredPoint>* points = parser.parseFile();
+	
+	
+	
+	// ==== Testing of Gonzalez ==== //
+	cout << "\n-----------------\nTesting Gonzalez: \n";
+
+	Gonzalez::GonzalezReturnValues* returnValues = Gonzalez::makeGonzalez(points, numberOfCluster);
+
+	cout << "MaxRadius (PlainGonzalez): " << returnValues->maxRadius << endl;
+	/*
+	cout << "Centers: " << endl;
+	// Centren anzeigen
+	for (int i = 0; i < (int) returnValues->centers->size(); i++){
+		cout << "\t- " << returnValues->centers->at(i).toString() << endl;
+	}*/
+
+
+	// ==== Testing of MaxRadius function ==== //
+	double maxRadiusViaFunction = redclustering::calculateMaxRadius(*returnValues->clusteredPoints, numberOfCluster);
+
+	cout << "MaxRadius (Funktion): " << maxRadiusViaFunction << endl;
+
+
+
+
+
+
+	// ==== Testing graph Buildup ==== //
+	// Get new Points
+	string unfairHandpointsFile = "Data/RandomGenerated/Unfair-Twocolor-2D.txt";
+	parser =  PointParser(unfairHandpointsFile);
+	vector<ColoredPoint>* unfairPoints = parser.parseFile();
+	
+	// Buildup Graph
+	Graph g;
+	fairlettFinder::GraphData gData;
+	CapacityMap capacity(g);
+
+	// Add Nodes
+	fairlettFinder::addNodesToGraph(g, gData, unfairPoints);
+
+	// Add Arcs
+	fairlettFinder::addArcsToGraph(g, gData, 15, unfairPoints);
+
+	// Add Capacities
+	fairlettFinder::addCapacitiesToGraph(capacity, gData);
+
+	// Get Max Flow Value
+	int maxFlowValue = fairlettFinder::getMaxFlow(g, capacity, gData);
+
+	// Get Flow itself
+	Flow* flow = fairlettFinder::calculateFlow(g, capacity, gData);
+
+	// Print outcome
+	fairlettFinder::printFlow(*flow, g, capacity, gData);
+	printf("Value of this Flow is %d\n", maxFlowValue);
+
+	// Flow von Kante nehmen
+	printf("Flow der ersten Haupt-Kante: %d\n", fairlettFinder::getFlowOfArc(*flow, gData.mainArcs.at(0)));
+	printf("Flow der zweiten Haupt-Kante: %d\n", fairlettFinder::getFlowOfArc(*flow, gData.mainArcs.at(1)));
+	
+
+	delete(flow);
+
+	
+
+
+
+	// ==== More Testing ==== //
+	cout << "\n===== Testing Radius checker =====\n" << endl;
+
+	// Farben richtig rum haben
+	fairlettFinder::makeCritFeatureSmalestFirst(unfairPoints);
+
+	// Kleine Radius Testen
+	double radSmall = 5;
+	printf("Gibt es ein erfolgreiches Matching bei r = %f? \t%d\n", radSmall, fairlettFinder::checkRadius(unfairPoints, radSmall));
+
+	// Mittleren Radius Testen
+	double radMedium = 20;
+	printf("Gibt es ein erfolgreiches Matching bei r = %f? \t%d\n", radMedium, fairlettFinder::checkRadius(unfairPoints, radMedium));
+
+	// Großen Radius Testen
+	double radBig = 50;
+	printf("Gibt es ein erfolgreiches Matching bei r = %f? \t%d\n", radBig, fairlettFinder::checkRadius(unfairPoints, radBig));
+
+	// Optimalen Radius finden
+	double optRad = fairlettFinder::findPotentionalRadius(unfairPoints);
+
+	printf("Der Optimale Radius, bei dem Fairlets gebildet werden können ist %f\n", optRad);
+
+
+	/*
+	// Calculate all possible Radii
+	vector<double>* potRadii = fairlettFinder::calculateAllRadii(unfairPoints);
+
+	// Print Radii
+	cout << "All Possible Radii:" << endl;
+	for (size_t i = 0; i < potRadii->size(); i++){
+		cout << potRadii->at(i) << endl;
+	}
+
+	delete potRadii;
+	*/
+
+	/*
+	// ==== Testing Color-Filter ==== //	
+	cout << "\n===== Testing Colorfilter =====\n" << endl;
+
+	// Fehler reinbringen
+	unfairPoints->at(0).setColor(GREEN);
+	unfairPoints->at(1).setColor(GREEN);
+	unfairPoints->at(3).setColor(GREEN);
+
+	// Filtern und Richtig mappen
+	fairlettFinder::makeCritFeatureSmalestFirst(unfairPoints);
+
+	vector<ColoredPoint>* redPoints = fairlettFinder::getPointsOfColor(unfairPoints, RED);
+	vector<ColoredPoint>* bluePoints = fairlettFinder::getPointsOfColor(unfairPoints, BLUE);
+
+	
+
+	cout << "\nSize-Comparison:" << endl;
+	cout << "\nAll Points: " << unfairPoints->size() << endl;
+	cout << "\nRed Points: " << redPoints->size() << endl;
+	cout << "\nBlue Points: " << bluePoints->size() << endl;
+
+
+	delete redPoints;
+	delete bluePoints;
+	*/
+
+
+	/*
+	cout << "Original Vector before filtering:" << endl;
+	for (size_t i = 0; i < unfairPoints->size(); i++){
+		cout << unfairPoints->at(i).toString() << endl;
+	}
+
+	// Filtern
+	vector<ColoredPoint>* redPoints = fairlettFinder::getPointsOfColor(unfairPoints, RED);
+	vector<ColoredPoint>* bluePoints = fairlettFinder::getPointsOfColor(unfairPoints, BLUE);
+
+	cout << "\nOriginal Vector after filtering:" << endl;
+	for (size_t i = 0; i < unfairPoints->size(); i++){
+		cout << unfairPoints->at(i).toString() << endl;
+	}
+
+	cout << "\nFiltered RedVector after filtering:" << endl;
+	for (size_t i = 0; i < redPoints->size(); i++){
+		cout << redPoints->at(i).toString() << endl;
+	}
+
+	cout << "\nSize-Comparison:" << endl;
+	cout << "\nAll Points: " << unfairPoints->size() << endl;
+	cout << "\nRed Points: " << redPoints->size() << endl;
+	cout << "\nBlue Points: " << bluePoints->size() << endl;
+
+
+	delete redPoints;
+	delete bluePoints;
+*/
+
+
+
+	// ==== Write Clusterdata into File ==== //
+
+	cout << "\n-----------------\nWrite Data into File:\n";
+
+	ofstream outputFile = ofstream(outputFileName, ios::out);
+
+	if(!outputFile.good()){
+		cout << "Error opening Outputfile" << endl;
+	} else{
+		// max Radius an den Anfang packen
+		outputFile << "maxRadius," << returnValues->maxRadius << endl;
+
+		// Punte abspeichern
+		for (int i = 0; i < (int) returnValues->clusteredPoints->size(); i++){
+			outputFile << returnValues->clusteredPoints->at(i).toCSV() << endl;
+		}
+	}
+
+	// ==== Clear-Up ==== //
+
+	// vector für Punkte wieder Freigeben
+	delete points;
+	delete unfairPoints;
+
+	// Struct für Gonzaleswerte wieder freigeben
+	Gonzalez::deleteGonzalezReturns(returnValues);
+
+	cout << "\n-----------------\nEnd Program" << endl;
+}
